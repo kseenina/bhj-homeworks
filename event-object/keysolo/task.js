c094a2f709bf7af6b2d3c2f43960f5f7951511ec
Ksenia Timokhina <kseenina@gmail.com>
2026-07-09 22:53:17 +0300

    Task 1.3 keysolo completed

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,15 +17,17 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+      document.addEventListener('keydown', (event) => {
+          const required = this.currentSymbol.textContent.toLowerCase();
+          const pressed = event.key.toLowerCase();
+          if (required === pressed) {
+              this.success();
+          } else {
+              this.fail();
+          }
+      });
   }
+
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
